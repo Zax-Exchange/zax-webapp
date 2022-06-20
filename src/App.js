9101cccbc674612ff299b941552c3a8fd68fab9d
Joseph Cheng <josephcheng@google.com>
2022-06-20 01:18:26 -0700

    added graphql files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+import { ApolloProvider, useQuery, gql } from '@apollo/client';
+
+const testQuery = gql`
+  query BookQuery {
+    books {
+      author
+    }
+  }
+`;
 
 function App() {
+  const {loading, data, error} = useQuery(testQuery);
+
+  if (data) {
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
---
+++ src/Services/GraphqlService.js
@@ -0,0 +1,5 @@
+function GraphqlService() {
+    
+}
+
+export default GraphqlService;
