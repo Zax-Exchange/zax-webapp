77b1b2f5672c5bbeea1ef00b022c5a8df4454e74
Joseph Cheng <josephcheng@MacBook-Pro-2.local>
2023-02-16 21:46:22 -0800

    changed sessionStorage to localStorage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -4,18 +4,8 @@ import React, { createContext, useReducer } from "react";
 import { EVENT_ACTION, EVENT_CATEGORY } from "../analytics/constants";
 import { LoggedInUser } from "../generated/graphql";
 import ReactGA from "react-ga4";
+import { BroadcastChannel } from "broadcast-channel";
 
-/**
- * user {
- *  id
- *  companyId
- *  isVendor
- *  power
- *  status
- *  name
- *  email
- * }
- */
 type SessionState = {
   user: LoggedInUser | null;
 };
@@ -23,11 +13,11 @@ const initialState = {
   user: null,
 };
 
-if (sessionStorage.getItem("token")) {
-  const decoded = jwtDecode(sessionStorage.getItem("token")!) as any;
+if (localStorage.getItem("token")) {
+  const decoded = jwtDecode(localStorage.getItem("token")!) as any;
 
   if (decoded.exp * 1000 < Date.now()) {
-    sessionStorage.removeItem("token");
+    localStorage.removeItem("token");
   } else {
     initialState.user = decoded;
   }
@@ -67,7 +57,7 @@ const AuthProvider = (props: any) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   const login = (userData: LoggedInUser) => {
-    sessionStorage.setItem("token", userData.token);
+    localStorage.setItem("token", userData.token);
     dispatch({
       type: "LOGIN",
       payload: userData,
@@ -75,7 +65,7 @@ const AuthProvider = (props: any) => {
   };
 
   const logout = () => {
-    sessionStorage.removeItem("token");
+    localStorage.removeItem("token");
     dispatch({ type: "LOGOUT" });
     ReactGA.event({
       action: EVENT_ACTION.LOGOUT,
