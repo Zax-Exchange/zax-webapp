4184363b8c98dd3c4840c79b7357c20eecd6d91b
Joseph Cheng <josephcheng@MacBook-Pro-2.local>
2023-02-16 21:46:22 -0800

    changed sessionStorage to localStorage

--- src/app/Projects/customer/CustomerProjects.tsx
+++ src/app/Projects/customer/CustomerProjects.tsx
@@ -37,7 +37,7 @@ const CustomerProjects = () => {
         userId,
       },
     },
-    fetchPolicy: "no-cache",
+    fetchPolicy: "network-only",
   });
 
   const [isProjectPageLoading, setIsProjectPageLoading] = useState(false);
