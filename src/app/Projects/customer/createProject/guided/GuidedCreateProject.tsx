a3136b43de5a5ea2bf73dc5b96fb7d1b3b47e4cd
Joseph Cheng <josephcheng@MacBook-Pro-2.local>
2023-02-23 01:36:23 -0800

    test commit with trivial change

--- src/app/Projects/customer/createProject/guided/GuidedCreateProject.tsx
+++ src/app/Projects/customer/createProject/guided/GuidedCreateProject.tsx
@@ -103,7 +103,7 @@ export default function GuidedCreateProject() {
   //     setSnackbarOpen(true);
   //   }
   //   return () => {
-  //     console.log(projectCreated);
+
   //   };
   // }, [projectCreated]);
 
