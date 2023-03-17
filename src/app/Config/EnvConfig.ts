f8b2985692bffd5e7323cb614097de967ad12d87
William Sun <sunwilliam@gmail.com>
2023-03-16 23:39:57 -0700

    Removed test suffix from env variable

--- src/app/Config/EnvConfig.ts
+++ src/app/Config/EnvConfig.ts
@@ -5,8 +5,8 @@ export class EnvConfig implements ZaxConfig {
   notificationServiceUrl = process.env.REACT_APP_NOTIFICATION_SERVICE_URL!;
   streamApiKey = process.env.REACT_APP_STREAM_API_KEY!;
   streamAppId = process.env.REACT_APP_STREAM_APP_ID!;
-  cloudfrontUrl =  process.env.REACT_APP_STATIC_CLOUDFRONT_URL!;
-  stripePublishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY_TEST!;
+  cloudfrontUrl = process.env.REACT_APP_STATIC_CLOUDFRONT_URL!;
+  stripePublishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY!;
   googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY!;
 }
 const envConfig = new EnvConfig() as ZaxConfig;
